e5766a837d65bb7bb6b81cca934eefb7c23cdd7a
pungkursari1088 <chrono.4ngel@gmail.com>
2021-01-02 07:23:47 +0700

    first commit

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,13 @@
+<?php
+class Welcome extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+        $this->load->view('admin/dashboard');
+    }
+}
---
+++ application/views/_partials/footer.php
@@ -0,0 +1,6 @@
+    <footer class="main-footer">
+      <div class="float-right d-none d-sm-inline">
+        <?php echo Date(VERSION) ?>
+      </div>
+      <strong>Copyright &copy; 2014-<?php echo Date('Y') ?> <a href="<?php echo base_url(''); ?>"> <?php echo SITE_NAME ?></a>.</strong> All rights reserved.
+    </footer>
